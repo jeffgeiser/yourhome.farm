6cbbbc6a610656d7f0890520bacad8045399083f
jeffgeiser <jeff@thegeisers.com>
2025-04-25 21:29:30 -0400

    Update Next.js configuration for Netlify deployment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: {
+    domains: ['yourhome.farm'],
+  },
   webpack: (config, { dev, isServer }) => {
     if (dev && !isServer) {
       config.cache = false;
